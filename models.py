ba12aeb1ddb7e02ccfa58751375c0b5eab59ea2c
Abdullah Ahmed <abdullah@softcomnetworks.com>
2020-07-23 21:06:41 +0100

    Fixed PEP8 Styling
    Modified code to comply with PEP8. Except Line 27 - no neat way to fix E501

--- models.py
+++ models.py
@@ -1,18 +1,19 @@
 import csv
 from datetime import datetime
 
+
 class Data:
     def __init__(self, filename="data.csv"):
         self._filename = filename
         self._data, self.line_count = self._read()
         self._header = self._read_header()
 
-    def writeRecord(self,**items):
+    def writeRecord(self, **items):
         args_dict = {}
         items_dict = items
 
         for item in self._header:
-            if not item in items_dict and item != 'date':
+            if item not in items_dict and item != 'date':
                 raise AttributeError(f"required argument '{item}' missing")
             if item == 'date':
                 args_dict[item] = datetime.today().strftime('%Y-%m-%d')
@@ -21,7 +22,7 @@ class Data:
                 del items_dict[item]
         if items_dict != {}:
             for item in items_dict.keys():
-                print(f"[WARN] '{item}' does not exist in file HEADER. Skipping write")
+                print(f"[WARN] '{item}' does not exist in file HEADER. Skipping write")  # noqa: E501
 
         self._write(args_dict)
 
@@ -29,26 +30,24 @@ class Data:
         line = 0
         data_ret = []
         with open(self._filename, mode='r') as csv_file:
-            reader = csv.DictReader(csv_file) 
+            reader = csv.DictReader(csv_file)
             for row in reader:
                 data_ret.append(row)
                 line += 1
         return data_ret, line
-    
+
     def _read_header(self):
         with open(self._filename, mode='r') as csv_file:
             reader = csv.reader(csv_file)
             header = next(reader, None)
         return header
-    
+
     def _write(self, data_dict):
         labels = data_dict.keys()
         with open(self._filename, mode='a') as csv_file:
-            writer = csv.DictWriter(csv_file,fieldnames=labels)
+            writer = csv.DictWriter(csv_file, fieldnames=labels)
             writer.writerow(data_dict)
-
 
     @property
     def data(self):
         return getattr(self, '_data')
-
